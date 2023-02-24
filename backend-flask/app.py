2f2a2a77774ab12a95c4acb9af24868073c59412
Mr-Rimmer <awsrat@btinternet.com>
2023-02-24 18:44:36 +0000

    Changes to code

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 import os
 
 from services.home_activities import *
-from services.notification_activity import *
+from services.notifications_activity import *
 from services.user_activities import *
 from services.create_activity import *
 from services.create_reply import *
@@ -62,13 +62,13 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
-def data_notification():
+def data_home():
   data = HomeActivities.run()
   return data, 200
 
   @app.route("/api/activities/notifications", methods=['GET'])
-def data_home():
-  data = NotificationActivities.run()
+def data_notifications():
+  data = NotificationsActivities.run()
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
