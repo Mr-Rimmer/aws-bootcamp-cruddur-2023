95c4a3356c464d544b8870920d2462ff66a1e645
Mr-Rimmer <124871057+Mr-Rimmer@users.noreply.github.com>
2023-03-13 14:26:33 +0000

    Update HomeFeedPage.js
    add authtoken line

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,6 +1,8 @@
 import './HomeFeedPage.css';
 import React from "react";
+
 import { Auth } from 'aws-amplify';
+
 import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
@@ -23,6 +25,9 @@ export default function HomeFeedPage() {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
       const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },
         method: "GET"
       });
       let resJson = await res.json();
